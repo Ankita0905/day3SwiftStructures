f3061bd70c5e83b33a86c38a3ab6c44cc80f747b
ankita jain <ankita.jain12342@gmail.com>
2019-10-10 21:07:23 -0400

    example

--- day3SwiftStructures/StudentStructure.swift
+++ day3SwiftStructures/StudentStructure.swift
@@ -11,16 +11,29 @@ import Foundation
 struct Student {
     var studentId: Int
     var studentName:String
+    var marks:[String:Float]
+    var totalMarks:Int
+    var per:Float
+    var result:String //fail if per<50 else pass
     
    // mutating func setValu()
     //{
       //  self.studentId=100
         //self.studentName="ankita"
     //}
+    func caltotalMarks() {
+       // totalMarks=
+    }
     
     func printData()
     {
-        print(studentId,studentName)
+        print(studentId)
+        print(studentName)
+        print(marks)
+        print(totalMarks)
+        print(per)
+        print(result)
+        
     }
 }
 
