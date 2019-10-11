49b7a43f936e87b72db3bd0f88f6e52aba448243
ankita jain <ankita.jain12342@gmail.com>
2019-10-11 19:28:43 -0400

    creating dictionary of objects and printing data by looping through

--- day3SwiftStructures/main.swift
+++ day3SwiftStructures/main.swift
@@ -36,16 +36,41 @@ if let x=a
 }
 
 
-var s1=Student(studentId: 1,studentName: "Ankita",marks: ["M1":50,"M2":60,"M3":67,"M4":78,"M5":80])
-
-s1.calTotalMarks()
-s1.percentage()
-s1.finalResult()
-s1.printData()
+//var s1=Student(studentId: 1,studentName: "Ankita",marks: ["M1":50,"M2":60,"M3":67,"M4":78,"M5":80])
+//
+//s1.calTotalMarks()
+//s1.percentage()
+//s1.finalResult()
+//s1.printData()
 
 var f1=Faculty(facultyId: 1, firstName: "Ankita", lastName: "Jain", basicSalary: 1000, bonus: 50)
-f1.printData()
-f1.calsalary()
+//f1.printData()
+//f1.calsalary()
+
+var f2=Faculty(facultyId: 2, firstName: "Kamal", lastName: "Kaur", basicSalary: 1020, bonus: 50)
+//f2.printData()
+//f2.calsalary()
+
+var f3=Faculty(facultyId: 3, firstName: "Karan", lastName: "Kumar", basicSalary: 1030, bonus: 50)
+//f3.printData()
+//f3.calsalary()
+
+var f4=Faculty(facultyId: 4, firstName: "Sandeep", lastName: "Singh", basicSalary: 1100, bonus: 50)
+//f4.printData()
+//f4.calsalary()
+
+var faculties=Dictionary<Int,Faculty>()
+faculties.updateValue(f1, forKey: f1.facultyId!)
+faculties.updateValue(f2, forKey: f2.facultyId!)
+faculties.updateValue(f3, forKey: f3.facultyId!)
+faculties.updateValue(f4, forKey: f4.facultyId!)
+
+for f in faculties
+{
+    f.value.printData()
+    f.value.calsalary()
+    print("---------------------------------------")
+}
 /*var f1=Faculty()
 //f1.setValues()
 f1.facultyId=1
